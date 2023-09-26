358a20fbf2e1242cb6ff477cd92dbfa6b2cd5d0e
Erich Simon <eprosimon@gmail.com>
2023-09-27 00:02:20 +0100

    Some progress but still not there

--- checks/version.go
+++ checks/version.go
@@ -1,13 +1,113 @@
 package checks
 
-const (
-	macReleaseURL          = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-macos-1/distribution_groups/release/public_releases?scope=tester"
-	macBetaURL             = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-macos/distribution_groups/beta/public_releases?scope=tester"
-	windowsReleaseURL      = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-windows-1/distribution_groups/release/public_releases?scope=tester"
-	windowsBetaURL         = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-windows/distribution_groups/beta/public_releases?scope=tester"
-	linuxReleaseURL        = "https://pkg.cloudflareclient.com/"
-	windowsDownloadURL     = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-windows-1/distribution_groups/release"
-	windowsBetaDownloadURL = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-windows/distribution_groups/beta"
-	macDownloadURL         = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-macos-1/distribution_groups/release"
-	macBetaDownloadURL     = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-macos/distribution_groups/beta"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/peakefficiency/warp-diag-toolkit/data"
 )
+
+var VersionCheckResult = data.CheckResult{}
+
+const (
+	MacReleaseURL          = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-macos-1/distribution_groups/release/public_releases?scope=tester"
+	MacBetaURL             = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-macos/distribution_groups/beta/public_releases?scope=tester"
+	WindowsReleaseURL      = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-windows-1/distribution_groups/release/public_releases?scope=tester"
+	WindowsBetaURL         = "https://install.appcenter.ms/api/v0.1/apps/cloudflare/1.1.1.1-windows/distribution_groups/beta/public_releases?scope=tester"
+	LinuxPKGurl            = "https://pkg.cloudflareclient.com/"
+	WindowsDownloadURL     = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-windows-1/distribution_groups/release"
+	WindowsBetaDownloadURL = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-windows/distribution_groups/beta"
+	MacDownloadURL         = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-macos-1/distribution_groups/release"
+	MacBetaDownloadURL     = "https://install.appcenter.ms/orgs/cloudflare/apps/1.1.1.1-macos/distribution_groups/beta"
+)
+
+const (
+	ForBeta    = true
+	ForRelease = false
+)
+
+type Release struct {
+	ID              int       `json:"id"`
+	ShortVersion    string    `json:"short_version"`
+	Version         string    `json:"version"`
+	UploadedAt      time.Time `json:"uploaded_at"`
+	MandatoryUpdate bool      `json:"mandatory_update"`
+	Enabled         bool      `json:"enabled"`
+}
+
+func fetchLatestVersionURL(beta bool) (string, error) {
+	switch data.Info.PlatformType {
+	case "windows":
+		if beta {
+			return WindowsBetaURL, nil
+		}
+		return WindowsReleaseURL, nil
+	case "mac":
+		if beta {
+			return MacBetaURL, nil
+		}
+		return MacReleaseURL, nil
+	case "linux":
+		return LinuxPKGurl, nil
+	default:
+		return "", fmt.Errorf("unknown platform type")
+	}
+}
+
+//helper function http call to get latest release which is the first json object in the response.
+
+func FetchLatestVersion(beta bool) (string, error) {
+	if data.Info.PlatformType == "linux" {
+		return "", nil
+	}
+
+	url, err := fetchLatestVersionURL(beta)
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 1,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch latest version: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	var releases []Release
+	err = json.Unmarshal(bodyBytes, &releases)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode JSON response: %v", err)
+	}
+
+	if len(releases) == 0 {
+		return "", fmt.Errorf("no releases found")
+	}
+
+	if data.Info.PlatformType == "windows" {
+		return releases[0].Version, nil
+	} else {
+		return releases[0].ShortVersion, nil
+	}
+}
--- checks/version_test.go
+++ checks/version_test.go
@@ -1,1 +1,35 @@
 package checks_test
+
+import (
+	"testing"
+
+	"github.com/peakefficiency/warp-diag-toolkit/checks"
+	"github.com/peakefficiency/warp-diag-toolkit/data"
+)
+
+// testing not parrelle
+func TestGetLatestReleaseVersionMac(t *testing.T) {
+
+	data.Info.PlatformType = "mac"
+	want := "2023.7.159.0"
+	got, err := checks.FetchLatestVersion(checks.ForRelease)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestGetLatestBetaVersionWindows(t *testing.T) {
+
+	data.Info.PlatformType = "windows"
+	want := "2023.9.107.1"
+	got, err := checks.FetchLatestVersion(checks.ForBeta)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
